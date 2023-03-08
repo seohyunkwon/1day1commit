83cd557293f590f1701712b1d83d81582094493f
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-03-08 20:55:04 +0900

    [Bronze I] Title: 소인수분해, Time: 156 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while(n>1) {
+        	for(int i=2; i<=n; i++) {
+        		if(n%i==0) {
+        			System.out.println(i);
+        			n/=i;
+        			break;
+        		}
+        	}
+        }
+    
+    
+    
+    }
+}
