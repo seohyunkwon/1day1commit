0be67c4823aad893246c03f37730c91b96a1f34d
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-03-10 12:01:25 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 128 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+public static void main(String[] args) throws IOException {
+	int[][] arr = new int[15][15];
+	for(int i=0; i<15; i++) {
+		arr[0][i]=i;
+		arr[i][1]=1;
+	}
+	for(int i=1; i<15; i++) {
+		for(int j=2; j<15; j++) {
+			arr[i][j] = arr[i-1][j]+arr[i][j-1];
+		}
+	}
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int t = Integer.parseInt(br.readLine());
+	for(int i=0; i<t; i++) {
+		int k = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(arr[k][n]);
+	}
+	
+	
+	}
+}
