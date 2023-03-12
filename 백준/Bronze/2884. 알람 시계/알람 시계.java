274cf0685afa143534e05a82c9faaffdbba8c8c6
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-03-12 22:07:32 +0900

    [Bronze III] Title: 알람 시계, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        if(M>=45) {System.out.println(H+" "+(M-45));}
+        else {
+            if(H==0) System.out.println(23+" "+(15+M));
+            else System.out.println((H-1)+" "+(15+M));}
+    }
+}
