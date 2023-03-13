5e95cd7bdb48f1c434682ce101e52d62842c1c66
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-03-13 15:00:25 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14312 KB -BaekjoonHub
    ArrayList를 사용한 풀이

--- 백준/Silver/1316. 그룹 단어 체커/another.java
+++ 백준/Silver/1316. 그룹 단어 체커/another.java
@@ -1,1 +1,29 @@
 
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for(int i=0; i<n; i++) {
+			ArrayList<Character> list = new ArrayList<Character>();		
+			String s = br.readLine();
+			for(int j=0; j<s.length(); j++) {
+				if(list.contains(s.charAt(j))) {
+					if(s.charAt(j)==s.charAt(j-1)) {
+						list.add(s.charAt(j));
+					} else {
+						list.add(s.charAt(j));
+						break;
+					}
+				} else {
+					list.add(s.charAt(j));
+				}
+				if(j==s.length()-1)cnt++;
+			}
+
+		}
+		System.out.println(cnt);
+	}
+}
