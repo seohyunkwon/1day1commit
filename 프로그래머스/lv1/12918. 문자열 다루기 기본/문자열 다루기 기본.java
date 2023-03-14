109cd3610e8fc57420952f5b6b34d1bfadb7b020
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-03-14 12:04:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public boolean solution(String s) {
+        if(s.length()==4 || s.length()==6){
+            try{ Integer.parseInt(s); return true;} catch (Exception e) {return false;}
+        } else return false;
+    }
+}
