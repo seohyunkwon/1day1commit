b62dd7304e71815858f10f75025052bd7dbe51d6
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-04-11 12:08:20 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,15 +2,19 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+	public static boolean[] visited;
+	public static ArrayList<ArrayList<Integer>> list;
 	public static void main(String[] args) throws IOException {
+		// 컴퓨터의 갯수와 링크 갯수 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
-		
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+		// 컴퓨터의 갯수(인덱스 1부터 취급)만큼 list 생성
+		list = new ArrayList<ArrayList<Integer>>();
 		for(int i=0; i<=n; i++) {
 			list.add(new ArrayList<Integer>());
 		}
+		// 링크 갯수만큼 입력받고 arraylist에 담아주기
 		for(int i=0; i<m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -18,29 +22,22 @@ class Main{
 			list.get(a).add(b);
 			list.get(b).add(a);
 		}
-		boolean[] visited = new boolean[n+1];
-		Stack<Integer> st = new Stack<Integer>();
-	
-		st.push(1);
-		visited[1]=true;
-		while(!st.isEmpty()) {
-			int idx = st.pop();
-			for(int i=0; i<list.get(idx).size(); i++) {
-				int y = list.get(idx).get(i);
-				if(!visited[y]) {
-					st.push(y);
-					visited[y]=true;
-				}
-			}
-		}
+		// dfs 실행하기 위한 boolean 배열
+		visited = new boolean[n+1];
+		dfs(1);
 		int cnt = -1;
 		for(boolean b:visited) {
 			if(b) cnt++;
 		}
 		System.out.println(cnt);
-		
-		
-		
-		
 	}
+	
+	public static void dfs(int x) {
+		visited[x]=true;
+		for(int i=0; i<list.get(x).size(); i++) {
+			int y = list.get(x).get(i);
+			if(!visited[y]) dfs(y);
+		}
+	}
+	
 }
