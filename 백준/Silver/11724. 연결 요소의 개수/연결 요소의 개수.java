3d167399db61c61251724a5309b881982c28103c
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-04-12 09:35:10 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 708 ms, Memory: 141420 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,10 +1,8 @@
 import java.io.*;
-import java.time.Year;
 import java.util.*;
 
 public class Main {
     static int n, m;
-    static int cnt = 0;
     static ArrayList<ArrayList<Integer>> list;
     static boolean[] visited;
 
@@ -23,33 +21,22 @@ public class Main {
         	int b = Integer.parseInt(st.nextToken());
         	list.get(a).add(b);
         	list.get(b).add(a);
-        	list.get(a).sort((k,j)->k-j);
-        	list.get(b).sort((k,j)->k-j);
         }
         visited = new boolean[n+1];
-        int k = searchVisited();
-        while(k<=n) {
-        	dfs(k);
-        	k = searchVisited();
+        int cnt = 0;
+        for(int i=1; i<=n; i++) {
+        	if(!visited[i]) {
+        		dfs(i);
+        		cnt++;
+        	}
         }
         System.out.println(cnt);
     }
 
     public static void dfs(int x) {
-    	if(x<1 || x>n) return;
     	visited[x]=true;
-    	for(int i=0; i<list.get(x).size(); i++) {
-    		int y = list.get(x).get(i);
-    		if(!visited[y]) dfs(y);
+    	for(int i:list.get(x)) {
+    		if(!visited[i]) dfs(i);
     	}
-    }
-    
-    public static int searchVisited() {
-    	int i=0;
-    	for(i=1; i<visited.length; i++) {
-    		if(!visited[i]) break;
-    	}
-    	if(i>0 && i<=n) cnt++;
-    	return i;
     }
 }
