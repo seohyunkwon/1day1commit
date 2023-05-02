d71c3ea635406eaeb06c95f80caac6cf367fe84d
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-05-02 09:12:23 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1784 ms, Memory: 99004 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int visit = 1;
+    static boolean[] visited;
+    static int[] result;
+    static  ArrayList<PriorityQueue<Integer>> graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new PriorityQueue<>(Collections.reverseOrder()));
+        }
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b =Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        visited = new boolean[n+1];
+        result = new int[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(r);
+        result[r-1] = visit++;
+        visited[r]=true;
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            while(!graph.get(tmp).isEmpty()){
+                int k = graph.get(tmp).poll();
+                if(!visited[k]){
+                    queue.add(k);
+                    visited[k] = true;
+                    result[k-1] = visit++;
+                }
+            }
+        }
+
+        Arrays.stream(result).forEach(System.out::println);
+
+    }
+}
