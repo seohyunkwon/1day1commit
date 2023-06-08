e0902da8285046e035814202388c4a7b6907a0fa
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-06-08 15:13:28 +0900

    [Silver V] Title: 나이순 정렬, Time: 1380 ms, Memory: 55216 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,39 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<Member> list = new ArrayList<Member>();
-		for(int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int age = Integer.parseInt(st.nextToken());
-			String name = st.nextToken();
-			list.add(new Member(age,name,i));
-		}
-		Collections.sort(list);
-		for(Member m : list) {
-			System.out.println(m);
-		}
-		
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Person[] arr = new Person[n];
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            arr[i] = new Person(age, name, i);
+        }
+        Arrays.sort(arr, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age!=o2.age?o1.age-o2.age:o1.idx-o2.idx;
+            }
+        });
+        for(Person p:arr){
+            System.out.println(p.age+" "+p.name);
+        }
+    }
 }
 
-class Member implements Comparable<Member>{
-	int age,num; String name;
-	@Override
-	public String toString() {
-		return age+" "+name;
-	}
-	Member(int age,String name,int num){
-		this.age=age;
-		this.name=name;
-		this.num=num;
-	}
-	@Override
-	public int compareTo(Member m) {
-		if(this.age==m.age) return this.num-m.num;
-		else return this.age-m.age;
-		}
+class Person{
+    int age;
+    String name;
+    int idx;
+    public Person(int age, String name, int idx){
+        this.age=age; this.name=name;   this.idx=idx;
+    }
 }
