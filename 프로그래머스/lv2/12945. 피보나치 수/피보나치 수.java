cb6620652bb15fc8a4aaff908cc7ae132b1520e7
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-10-06 22:56:13 +0900

    [level 2] Title: 피보나치 수, Time: 2.58 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] arr = new int[n+1];
-        arr[1] = 1; arr[2] = 1;
-        int answer = 0;
-        for(int i=3; i<=n; i++){
-            arr[i] += (arr[i-1]%1234567)+(arr[i-2]%1234567);
+        int[] arr = new int[100001];
+        arr[1] = 1;
+        for(int i=2; i<arr.length; i++){
+            arr[i] = (arr[i-2]+arr[i-1])%1234567;
+            if(i==n) break;
         }
-        return arr[n]%1234567;
+        return arr[n];
     }
 }
