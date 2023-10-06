8d960871db4f31c1c73e3ee2a318b87318732a31
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-10-07 00:00:51 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
-        for(int i=1; i<=Math.sqrt(yellow); i++){
-            int j = yellow/i;
-            if(i*j!=yellow) continue;
-            if(brown==(i+2)*2+(j+2)*2-4){
-                return new int[]{Math.max(i+2, j+2),Math.min(i+2, j+2)};
+        for(int i=1; i<brown; i++){
+            for(int j=1; j<brown; j++){
+                int x = i;
+                int y = j;
+                if(x*y-(x-2)*(y-2)==brown && (x-2)*(y-2)==yellow){
+                    return new int[]{y,x};
+                }
             }
+            
         }
         return answer;
     }
