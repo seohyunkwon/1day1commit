280c6dab1c7c2f3d2d2196616a8e9b41becca071
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2023-10-12 00:42:22 +0900

    [level 1] Title: K번째수, Time: 0.22 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length; i++){
+            int[] command = commands[i];
+            List<Integer> list = new ArrayList<>();
+            for(int j = command[0]-1; j<=command[1]-1; j++){
+                list.add(array[j]); 
+            }
+            Collections.sort(list);
+            answer[i] = list.get(command[2]-1); 
+        }
+        return answer;
+    }
+}
