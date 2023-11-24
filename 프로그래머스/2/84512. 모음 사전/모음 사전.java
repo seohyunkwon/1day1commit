b3c3464fe8a27c25c648b5783d8ece3620729603
SeohyunKwon <104131284+seohyunkwon@users.noreply.github.com>
2023-11-24 16:05:31 +0900

    [level 2] Title: 모음 사전, Time: 4.78 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-class Solution {
-    static List<String> list;
-    static String [] words = {"A", "E", "I", "O", "U"};
-    public int solution(String word) {
-        int answer = 0;
-        list = new ArrayList<>();
-        dfs("", 0);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            if (list.get(i).equals(word)) {
-                answer = i;
-                break;
-            }
-        }
-        return answer;
-    }
+import java.util.*;
 
-    static void dfs(String str, int len) {
-        list.add(str);
-        if (len == 5) return;
-        for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
+class Solution {
+    public List<String> list = new ArrayList<>();
+    public int solution(String word) {
+        String[] arr = {"A", "E", "I", "O", "U"};
+        dfs("", arr, 0);
+        return list.indexOf(word)+1;
+    }
+    
+    private void dfs(String str, String[] arr, int depth){
+        if(depth == 5 ) return;
+        for(int i=0; i<arr.length; i++){
+            list.add(str+arr[i]);
+            dfs(str+arr[i], arr, depth+1);
         }
     }
 }
