f7823a458099caad1e5fc609dfdbda9ec8627fb2
SeohyunKwon <104131284+seohyunkwon@users.noreply.github.com>
2024-01-16 00:20:47 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,43 +1,49 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Main{
-	public static boolean[] visited;
-	public static ArrayList<ArrayList<Integer>> list;
-	public static void main(String[] args) throws IOException {
-		// 컴퓨터의 갯수와 링크 갯수 입력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		// 컴퓨터의 갯수(인덱스 1부터 취급)만큼 list 생성
-		list = new ArrayList<ArrayList<Integer>>();
-		for(int i=0; i<=n; i++) {
-			list.add(new ArrayList<Integer>());
-		}
-		// 링크 갯수만큼 입력받고 arraylist에 담아주기
-		for(int i=0; i<m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list.get(a).add(b);
-			list.get(b).add(a);
-		}
-		// dfs 실행하기 위한 boolean 배열
-		visited = new boolean[n+1];
-		dfs(1);
-		int cnt = -1;
-		for(boolean b:visited) {
-			if(b) cnt++;
-		}
-		System.out.println(cnt);
-	}
-	
-	public static void dfs(int x) {
-		visited[x]=true;
-		for(int i=0; i<list.get(x).size(); i++) {
-			int y = list.get(x).get(i);
-			if(!visited[y]) dfs(y);
-		}
-	}
-	
+public class Main {
+    static int count = 0;
+    static boolean[] visited;
+    static  ArrayList<ArrayList<Integer>> graph;
+    static Queue<Integer> queue;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int i=0; i<m; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        visited = new boolean[n+1];
+        queue = new LinkedList<>();
+        bfs(1);
+        System.out.println(count);
+
+    }
+
+    public static void bfs(int n){
+        queue.add(n);
+        visited[n] = true;
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            for(int i=0; i<graph.get(tmp).size(); i++){
+                int j = graph.get(tmp).get(i);
+                if(!visited[j]){
+                    queue.add(j);
+                    visited[j] = true;
+                    count++;
+                }
+            }
+        }
+    }
+
 }
