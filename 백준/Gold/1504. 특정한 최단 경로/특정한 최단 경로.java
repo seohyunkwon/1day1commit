268389a3b9001434878229d9e57044e908d0574a
SeohyunKwon <104131284+seohyunkwon@users.noreply.github.com>
2024-05-03 16:06:53 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 644 ms, Memory: 62032 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,14 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int INF = 200_000_000;
+	static int N, INF = 200_000_000;
+	static int[][] dist, graph;
+	static PriorityQueue<Node> pq;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         
-        int[][] graph = new int[N + 1][N + 1];
+        graph = new int[N + 1][N + 1];
         
         for(int i = 1; i < N + 1; i++) {
         	Arrays.fill(graph[i], INF);
@@ -32,24 +34,47 @@ public class Main {
 			graph[i][i] = 0;
         }
         
-        for(int k = 1; k < N + 1; k++) {
-        	for(int i = 1; i < N + 1; i++) {
-        		for(int j = 1; j < N + 1; j++) {
-        			graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-        		}
-        	}
+        dist = new int[N + 1][N + 1];
+        for(int i = 1; i < N + 1; i++) {
+        	Arrays.fill(dist[i], INF);        	
         }
-        // 1 - a - b - N
-        // 1 - b - a - N
         
-        int tmp = Math.min(graph[1][v2]+graph[v2][v1]+graph[v1][N], graph[1][v1]+graph[v1][v2]+graph[v2][N]);
-        System.out.println(tmp >= INF ? -1 : tmp);
+        dijkstra(1);
+        dijkstra(v1);
+        dijkstra(v2);
         
+        int result = Math.min(dist[1][v1] + dist[v1][v2] + dist[v2][N], dist[1][v2] + dist[v2][v1] + dist[v1][N]);
+        
+//        for(int[] arr : dist) {
+//        	System.out.println(Arrays.toString(arr));
+//        }
+        
+        System.out.println(result >= INF ? -1 : result);
+        
+        
+    }
+    
+    static void dijkstra(int idx) {
+    	pq = new PriorityQueue<>((p1, p2) -> p1.dist - p2.dist);
+    	dist[idx][idx] = 0;
+    	pq.offer(new Node(idx, 0));
+    	while(!pq.isEmpty()) {
+    		Node node = pq.poll();
+    		
+    		for(int i = 1; i < N + 1; i++) {
+    			if(i == idx) continue;
+    			if (dist[idx][i] > dist[idx][node.idx] + graph[node.idx][i]) {
+                    dist[idx][i] = dist[idx][node.idx] + graph[node.idx][i];
+                    pq.offer(new Node(i, dist[idx][i]));
+                }
+    			
+    		}
+    	}
+    	
     }
     
     static class Node {
     	int idx, dist;
-    	boolean isVisitV1, isVisitV2;
 
 		public Node(int idx, int dist) {
 			this.idx = idx;
