1c785a4ec5c1f8d4ec1c15fb860936ad655f4ea6
SeohyunKwon <104131284+seohyunkwon@users.noreply.github.com>
2024-07-14 20:54:00 +0900

    [level 2] Title: [1차] 캐시, Time: 42.01 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,52 +1,28 @@
 import java.util.*;
+
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if(cacheSize==0) return cities.length*5;
+        if(cacheSize == 0) {
+            return cities.length * 5;
+        }
         int answer = 0;
-        PriorityQueue<Page> queue = new PriorityQueue<>((a,b)->a.recentlyUse-b.recentlyUse);
-        Set<String> set = new HashSet<>(); 
-        for(int i=0; i<cities.length; i++){
-            String s = cities[i].toLowerCase();
-            if(set.contains(s)) {
-                answer+=1;
-                queue.removeIf(a->a.city.equals(s));
-                queue.add(new Page(s,i));
-            } else {
-                if(queue.size()>=cacheSize) {
-                    Page page = queue.poll();
-                    set.remove(page.city);
-                }
-                queue.add(new Page(s, i));
-                set.add(s);
-                answer+=5;
+        Queue<String> q = new LinkedList<>();
+        for(int i = 0; i < cities.length; i++) {
+            if(q.contains(cities[i].toLowerCase())) {
+                answer++;
+                q.remove(cities[i].toLowerCase());
+                q.offer(cities[i].toLowerCase());
+                continue;
             }
+            
+            if(q.size() >= cacheSize) {
+                q.poll();
+            }
+            
+            q.offer(cities[i].toLowerCase());
+            
+            answer+= 5;
         }
         return answer;
-    }
-}
-
-class Page{
-    String city;
-    int recentlyUse;
-
-    public int getRecentlyUse() {
-        return recentlyUse;
-    }
-
-    public void setRecentlyUse(int recentlyUse) {
-        this.recentlyUse = recentlyUse;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Page(String city, int recentlyUse) {
-        this.city = city;
-        this.recentlyUse = recentlyUse;
     }
 }
