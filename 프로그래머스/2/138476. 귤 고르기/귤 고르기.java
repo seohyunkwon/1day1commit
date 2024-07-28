f5b589bf5012c7cff70bd88514c4f1ef0eeee420
SeohyunKwon <104131284+seohyunkwon@users.noreply.github.com>
2024-07-28 11:48:12 +0900

    [level 2] Title: 귤 고르기, Time: 56.84 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,20 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i:tangerine){
-            map.put(i, map.getOrDefault(i, 0)+1);
+        for(int i = 0; i < tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
-        for(int i:map.keySet()){
-            pq.add(new int[]{i, map.get(i)});
+        
+        PriorityQueue<Obj> pq = new PriorityQueue<>();
+        
+        for(Integer num : map.keySet()) {
+            pq.offer(new Obj(num, map.get(num)));
         }
-        while(k>0){
-            int[] tmp = pq.poll();
-            k -= tmp[1];
+        
+        while(k > 0) {
+            int tmp = pq.poll().cnt;
             answer++;
+            if(tmp <= k) {
+                k -= tmp;
+                continue;
+            }
+            break;
         }
+        
         return answer;
+    }
+    
+    class Obj implements Comparable<Obj> {
+        int num, cnt;
+        
+        Obj(int num, int cnt) {
+            this.num = num; this.cnt = cnt;
+        }
+        
+        @Override
+        public int compareTo(Obj obj) {
+            return obj.cnt - this.cnt;
+        }
     }
 }
